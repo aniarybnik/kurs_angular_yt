09f98cdb447aca307dae5a9f610fd8331585e1f6
aniarybnik <ania-rybnik1995@o2.pl>
2018-10-25 11:06:23 +0200

    Dodawanie listy zadań

--- yt/src/app/app.component.ts
+++ yt/src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'yt';
+
+  zadanie: string;
+  listaZadan: Array<string> = [];  // Tablica będzie przyjmowała same stringi
+  listaZadanWykonanych: Array<string> = [];
+  licznik = 0;
+
+  // kiedyDodac = false;
+
+  dodaj() {
+    // this.kiedyDodac = true;
+    this.listaZadan.push(this.zadanie); // Dodawanie do Tablicy kolejnych zadań
+    this.zadanie = '';
+  }
+
+  usun(i: string) {
+    this.listaZadan = this.listaZadan.filter( e => e !== i);
+  }
+
+  dodajZrobione(i: string) {
+    this.listaZadan = this.listaZadan.filter( e => e !== i);
+    this.listaZadanWykonanych.push(i);
+    this.licznik = this.listaZadanWykonanych.length;
+  }
 }
+
